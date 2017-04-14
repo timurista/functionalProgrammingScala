a2e69b0285f8719a87974e265c0ae6ab36ffefd4
Tim Urista <timothy.urista@gmail.com>
2017-04-13 20:48:23 -0700

    master update workspace.xmlAM in .idea/workspace.xmlAM

---
+++ src/main/scala/week3/Rational.scala
@@ -0,0 +1,49 @@
+package week3
+
+/**
+  * Created by tim.urista on 4/13/17.
+  */
+class Rational(n: Int, d: Int) {
+  require(d != 0, "denominator must be nonzero")
+
+  private val g = gcd(n.abs, d.abs)
+  val number = n / g
+  val denom = d / g
+
+  def this(n: Int) = this(n, 1) // auxiliary constructor
+
+  def + (that: Rational): Rational =
+    new Rational(
+      number * that.denom + that.number * denom,
+      denom * that.denom
+    )
+
+  def + (i: Int): Rational =
+    new Rational(number + i * denom, denom)
+
+  def - (that: Rational): Rational =
+    new Rational(
+      number * that.denom - that.number * denom,
+      denom * that.denom
+    )
+
+  def - (i: Int): Rational =
+    new Rational(number - i * denom, denom)
+
+  def * (that: Rational): Rational =
+    new Rational(number * that.number, denom * that.denom)
+
+  def * (i: Int): Rational =
+    new Rational(number * i, denom)
+
+  def / (that: Rational): Rational =
+    new Rational(number * that.denom, denom * that.number)
+
+  def / (i: Int): Rational =
+    new Rational(number, denom * i)
+
+  override def toString = number + "/" + denom
+
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
+}
